dfec1bdfe1da7c86f7053971049848f120b71b4f
Vyaas <saayvsaayv@gmail.com>
2018-06-24 03:28:27 -0700

    There were syntax errors. I eliminated as much as I could

--- blockchain/python scripts/curl_params.py
+++ blockchain/python scripts/curl_params.py
@@ -5,6 +5,4 @@ payload= {"address":"n1i3ToCvRr85Mfu3CRH7Bz4tuGGgrHaacn2"}
 headers = {'Content-Type: application/json'}
 r=requests.post("http://localhost:8685/v1/user/accountstate ", data=json.dumps(payload),headers= headers)
 
-print "r.text : ", r.text
-
-
+print ("r.text : ", r.text)
